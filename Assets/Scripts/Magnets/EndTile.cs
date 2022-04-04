938954b25ce58e5d37ed4005e487b775fccd7ccc
Biggu08 <q.winants@gmail.com>
2022-04-04 11:08:24 +0200

    UI

--- Assets/Scripts/Magnets/EndTile.cs
+++ Assets/Scripts/Magnets/EndTile.cs
@@ -3,31 +3,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace LaraGoLike
+public class EndTile : Tile
 {
-    public class EndTile : Tile
+    public static EndTile Instance;
+
+    private void Awake()
     {
-        Ray ray;
-        [SerializeField]
-        LayerMask layer_mask;
+        Instance = this;
+    }
 
-        private void Start()
-        {
-            ray = new Ray(transform.position, Vector3.up);
-        }
+    
+    
+    Ray ray;
+    [SerializeField]
+    LayerMask layer_mask;
 
-        private void Update()
+    private void Start()
+    {
+        ray = new Ray(transform.position, Vector3.up);
+    }
+
+    private void Update()
+    {
+        Debug.DrawRay(ray.origin,ray.direction,Color.yellow);
+        if (Physics.Raycast(ray, out RaycastHit hit, 1f, layer_mask, QueryTriggerInteraction.Ignore))
         {
-           
-            Debug.DrawRay(ray.origin,ray.direction,Color.yellow);
-            if (Physics.Raycast(ray, out RaycastHit hit, 1f, layer_mask, QueryTriggerInteraction.Ignore))
+            if (hit.collider != null && hit.collider.gameObject.TryGetComponent<Train>(out Train train))
             {
-                if (hit.collider != null && hit.collider.gameObject.TryGetComponent<Train>(out Train train))
-                {
-                    Debug.Log("C GAGNE YOUPI");
-                }
+                UIManager.Instance.WinUI();
             }
-        
         }
+    
     }
 }
+
